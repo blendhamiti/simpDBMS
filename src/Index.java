d20d1e0e637055c148e2e14578d808863b01820e
Blend Hamiti <b.hamiti.9@gmail.com>
2020-12-08 23:30:35 +0100

    implemented table index column row database connection

--- src/Index.java
+++ src/Index.java
@@ -1,8 +1,64 @@
-import java.util.TreeSet;
+import java.util.*;
 
 public class Index {
+    private String name;
+    private Column column;
+    private Map<Record,List<Address>> entries;
 
-    public Index(Table table) {
+    public Index(String name, Column column) {
+        this.name = name;
+        this.column = column;
+        entries = new TreeMap<>();
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public Column getColumn() {
+        return column;
+    }
+
+    private void fetchIndex() {
+    }
+
+    public void addEntry(Record record) {
+    }
+
+    public void updateEntry(Address address, Record record) {
+    }
+
+    public void removeEntry(Record record) {
+    }
+
+    public List<Address> getAddress(Record record) {
+        List<Address> addresses = new ArrayList<>();
+        for (Record currentRecord : entries.keySet()) {
+            if (currentRecord.equals(record))
+                addresses.addAll(entries.get(currentRecord));
+        }
+        return addresses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Index index = (Index) o;
+        return name.equals(index.name) &&
+                column.equals(index.column);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, column);
+    }
+
+    @Override
+    public String toString() {
+        return "Index{" +
+                "name='" + name + '\'' +
+                ", column=" + column +
+                '}';
     }
 }
--- src/Row.java
+++ src/Row.java
@@ -1,2 +1,50 @@
+import java.util.List;
+import java.util.Objects;
+
 public class Row {
+    private List<Record> records;
+
+    public Row(List<Record> records) {
+        this.records = records;
+    }
+
+    private void fetchRow() {
+    }
+    
+    public List<Record> getRecords() {
+        return records;
+    }
+    
+    public Record getRecord(Column column) {
+        return null;
+    }
+
+    public void addRecord(Column column, Record record) {
+    }
+
+    public void updateRecord(Column column, Record record) {
+    }
+
+    public void removeRecord(Column column) {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Row row = (Row) o;
+        return records.equals(row.records);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(records);
+    }
+
+    @Override
+    public String toString() {
+        return "Row{" +
+                "records=" + records +
+                '}';
+    }
 }
