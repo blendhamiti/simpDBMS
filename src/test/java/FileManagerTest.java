de10ee01c7d8bc1d7beea0591a0d06e7dcfad996
Blend Hamiti <b.hamiti.9@gmail.com>
2020-12-21 12:40:36 +0100

    added first test class

---
+++ src/test/java/FileManagerTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FileManagerTest {
+
+    @Test
+    public void getOrCreateDirectoryTest() {
+        Assert.assertTrue(true);
+    }
+}
